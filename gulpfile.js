f7d4ca72c33528805d1bdc30e26e171f7190ecf2
Helena Vieites <hvieites@gmail.com>
2021-09-27 03:01:43 -0300

    Correção -parte1

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass(){
     return gulp
     .src('scss/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
     .pipe(gulp.dest('css/'))
 }
 
